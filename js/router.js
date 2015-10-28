313b50d34d6f8ac619ec333f4231c1a4fa380395
Jonny Warren <jonnywarren06@gmail.com>
2015-10-28 16:26:21 -0400

    router written.  time to make sure it works.

--- js/router.js
+++ js/router.js
@@ -13,4 +13,65 @@ import {
 
 export default Backbone.Router.extend({
 
+	routes: {
+		''					      : 'redirectToBandName',
+		'bandName'   	    : 'showBandNames',
+		'bandProfile/:id' : 'showBandProfile'
+	},
+
+	initialize(appElement) {
+		this.$el = appElement;
+		this.collection = new BandCollection();
+	},
+
+	start() {
+		Backbone.history.start();
+		return this;
+	},
+
+	showSpinner() {
+		this.$el.html( Spinner() );
+	},
+
+	redirectToBandName() {
+		this.navigate('bandName', {
+			replace: true,
+			trigger: true
+		});
+	},
+
+	showBandNames() {
+		this.showSpinner();
+		this.collection.fetch().then(() => {
+			this.$el.html(
+				BandNameView(
+					this.collection.toJSON()
+				)
+			);
+		});
+	},
+
+	showBandProfile(id) {
+		let band = this.collection.get(id);
+
+		if (band) {
+			this.$el.html(
+				BandProfileView(
+					band.templateData()
+				)
+			);
+		} else {
+			this.showSpinner();
+			band = this.collection.add({objectId: id});
+			band.fetch().then(() => {
+				this.$el.html(
+					BandProfileView(
+						band.templateData()
+					)
+				);
+			});
+		}
+	}
+
+
 });
